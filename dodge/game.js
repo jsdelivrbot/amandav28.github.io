f302a653d8682330c1933342fc97263c3b5d5818
Amanda Vasconcelos <avasconcelos28@gmail.com>
2018-02-06 17:59:06 +0000

    Santiago Update

--- dodge/game.js
+++ dodge/game.js
@@ -1,48 +1,126 @@
 var isGameOver;
 var player;
 var playerImage;
-var enemy;
-var enemyImage;
+var homework;
+var homeworkImage;
+var responsibilities;
+var responsibilitiesImage;
 var backgroundImage;
+var essay;
+var essayImage;
+var summer;
+var summerImage;
+var mastering;
+var masteringImage;
 
 function preload(){
-    playerImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/N5uCbDu.png");
-    enemyImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/OdL0XPt.png");
-    backgroundImage =loadImage("https://surrogate.hackedu.us/i.imgur.com/aKQOg3G.png");
+    playerImage = loadImage("https://i.imgur.com/ZWtwGt6.png?3");
+    responsibilitiesImage= loadImage("https://i.imgur.com/WLlPM85.png?3");
+    homeworkImage = loadImage("https://i.imgur.com/WLlPM85.png?3");
+    essayImage = loadImage("https://i.imgur.com/KC3xpJ9.png?1");
+    summerImage = loadImage("https://i.imgur.com/p6k5vLX.png?1");
+    masteringImage = loadImage("https://i.imgur.com/qzpfts6.png?2");
+    backgroundImage =loadImage("https://i.imgur.com/SuD5BBT.jpg?1");
 }
 
 function setup() {
     isGameOver = false;
-    createCanvas(256, 256);
+    createCanvas(400, 400);
     player = createSprite(width / 2, height - (playerImage.height / 2), 0, 0);
     player.addImage(playerImage);
-    enemy = createSprite(width / 2, 0, 0, 0);
-    enemy.addImage(enemyImage);
-    enemy.rotationSpeed = 4.0;
+    homework = createSprite(width / 2, 0, 0, 0);
+    homework.addImage(homeworkImage);
+    homework.rotationSpeed = 5.5;
+    homework.position.x = random(5, width-5);
+    responsibilities = createSprite(width / 2, 0, 0, 0);
+    responsibilities.addImage(responsibilitiesImage);
+    responsibilities.position.x = random(5, width-5);
+    essay = createSprite(width / 2, 0, 0, 0);
+    essay.addImage(essayImage);
+    essay.rotationSpeed = 6.0;
+    essay.position.x = random(5, width-5);
+    summer = createSprite(width / 2, 0, 0, 0);
+    summer.addImage(summerImage);
+    summer.rotationSpeed = 5.3;
+    summer.position.x = random(5, width-5);
+    mastering = createSprite(width / 2, 0, 0, 0);
+    mastering.addImage(masteringImage);
+    mastering.position.x = random(5, width-5);
 }
 
 function draw () {
     if (isGameOver) {
         gameOver();
     } else {
-        if(enemy.overlap(player)) {
+        if(homework.overlap(player)) {
             isGameOver = true;
     }
+        if(responsibilities.overlap(player)) {
+            isGameOver = true;
+    }
+        if(essay.overlap(player)){
+            isGameover = true;
+    }
+        if(summer.overlap(player)) {
+            isGameover = true;
+    }
+        if(mastering.overlap(player)) {
+            isGameover = true;
+    }
+    
     background(backgroundImage);
+    
     if (keyDown(RIGHT_ARROW) && player.position.x < (width - (playerImage.width / 2))) {
-     player.position.x += 2;
+     player.position.x += 4;
     }
     
     if (keyDown(LEFT_ARROW) && player.position.x > (playerImage.width / 2)) {
-        player.position.x -= 2;
+        player.position.x -= 4;
     }
     
-    enemy.position.y = enemy.position.y + 3;
-    
-    if (enemy.position.y > height) {
-        enemy.position.y = 0;
-        enemy.position.x=random(5, width-5);
+    if (keyDown(UP_ARROW) && player.position.y < (height + (playerImage.height / 2))) {
+        player.position.y -= 4;
     }
+    
+    if (keyDown(DOWN_ARROW) && player.position.y > (playerImage.height / 2)) {
+        player.position.y += 4;
+    }
+    
+    homework.position.y = homework.position.y + 3;
+    
+    if (homework.position.y > height) {
+        homework.position.y = 0;
+        homework.position.x=random(5, width-5);
+    }
+    
+    responsibilities.position.y = responsibilities.position.y +3;
+    
+    if (responsibilities.position.y > height) {
+        responsibilities.position.y = 0;
+        responsibilities.position.x = random (5, width-5);
+    }
+    
+    essay.position.y = essay.position.y + 3;
+    
+    if (essay.position.y > height) {
+        essay.position.y = 0;
+        essay.position.x=random(5, width-5);
+    }
+    
+    mastering.position.y = mastering.position.y + 3;
+    
+    if (mastering.position.y > height) {
+        mastering.position.y = 0;
+        mastering.position.x=random(5, width-5);
+    }
+    
+    summer.position.y = summer.position.y + 3;
+    
+    if (summer.position.y > height) {
+        summer.position.y = 0;
+        summer.position.x=random(5, width-5);
+    }
+    
     drawSprites();
     }
 }
@@ -60,7 +138,15 @@ mouseClicked = function () {
      isGameOver = false;
      player.position.x = width / 2;
      player.position.y = height - (playerImage.height / 2);
-     enemy.position.x = width / 2;
-     enemy.position.y = 0;
+     homework.position.x = width / 2;
+     homework.position.y = 0;
+     responsibilities.position.x = width / 3;
+     responsibilities.position.y = 0;
+     essay.position.x = width / 3;
+     essay.position.y = 0;
+     summer.position.x = width / 3;
+     summer.position.y = 0;
+     mastering.position.x = width / 3;
+     mastering.position.y = 0;
     }
 }
